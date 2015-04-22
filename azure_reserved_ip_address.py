dfafd9c973e3b2b57195feb274441c5ab6cc4d2e
Darren Warner <darren@kwaaioak.com>
2015-04-22 14:30:27 -0700

    Add azure_reserved_ip_address module

--- azure_reserved_ip_address.py
+++ azure_reserved_ip_address.py
@@ -2,26 +2,26 @@
 
 DOCUMENTATION = '''
 ---
-module: azure_service
-short_description: create or terminate a cloud service in azure
+module: azure_ip_address
+short_description: create or delete a reserved ip address
 description:
-     - Creates or deletes cloud services. This module has a dependency on python-azure >= 0.7.1
+     - Creates or deletes reserved IP addresses. This module has a dependency on python-azure >= 0.7.1
 version_added: "1.9"
 options:
   name:
     description:
-      - name of the cloud service
+      - name of the reserved IP address
     required: true
+    default: null
+  label:
+    description:
+      - a label for the reserved IP address (up to 100 characters)
+    required: false
     default: null
   location:
     description:
       - the azure location to use (e.g. 'East US')
     required: true
-    default: null
-  affinity_group:
-    description:
-      - name of an existing affinity group to be associated with (cannot be used with location)
-    required: false
     default: null
   subscription_id:
     description:
@@ -52,7 +52,7 @@ options:
     aliases: []
   state:
     description:
-      - create or delete the cloud service
+      - create or delete the reserved IP address
     required: false
     default: 'present'
     aliases: []
@@ -67,15 +67,15 @@ EXAMPLES = '''
 
 # Provision virtual machine example
 - local_action:
-    module: azure_service
-    name: my-service
+    module: azure_ip_address
+    name: my-ip-address
     location: 'East US'
     wait: yes
 
-# Terminate virtual machine example
+# Delete reserved IP address
 - local_action:
-    module: azure_service
-    name: my-service
+    module: azure_ip_address
+    name: my-ip_address
     state: absent
 '''
 
@@ -125,9 +125,9 @@ def _wait_for_completion(azure, promise, wait_timeout, msg):
 
     raise WindowsAzureError('Timed out waiting for async operation ' + msg + ' "' + str(promise.request_id) + '" to complete.')
 
-def create_service(module, azure):
+def create_ip_address(module, azure):
     """
-    Create new service
+    Create new reserved IP address
 
     module : AnsibleModule object
     azure: authenticated azure ServiceManagementService object
@@ -136,44 +136,50 @@ def create_service(module, azure):
         True if a new servce was created, false otherwise
     """
     name = module.params.get('name')
+    label = module.params.get('label')
     location = module.params.get('location')
-    affinity_group = module.params.get('affinity_group')
     wait = module.boolean(module.params.get('wait'))
     wait_timeout = int(module.params.get('wait_timeout'))
 
     # Check if a deployment with the same name already exists
-    cloud_service_name_available = azure.check_hosted_service_name_availability(name)
-    if not cloud_service_name_available.result:
+    reserved_ip_address = None
+    try:
+        reserved_ip_address = azure.get_reserved_ip_address(name=name)
+    except WindowsAzureMissingResourceError as e:
+        pass  # no such reserved ip address
+    except WindowsAzureError as e:
+        module.fail_json(msg="failed to find the reserved IP address, error was: %s" % str(e))
+
+    if reserved_ip_address:
         changed = False
     else:
         changed = True
-        # Create cloud service if necessary
+        # Create reserved IP address if necessary
         try:
-            result = azure.create_hosted_service(service_name=name, label=name, location=location, affinity_group=affinity_group)
-            if (wait):
-                _wait_for_completion(azure, result, wait_timeout, "create_hosted_service")
+            result = azure.create_reserved_ip_address(name=name, label=label, location=location)
+            if wait:
+                _wait_for_completion(azure, result, wait_timeout, "create_reserved_ip_address")
+                reserved_ip_address = azure.get_reserved_ip_address(name=name)
         except WindowsAzureError as e:
-            module.fail_json(msg="failed to create the new service name: %s" % str(e))
+            module.fail_json(msg="failed to create the new reserved IP address: %s" % str(e))
 
-    try:
-        service = None
-        if (wait):
-            service = azure.get_hosted_service_properties(service_name=name)
-        return (changed, service)
-    except WindowsAzureError as e:
-        module.fail_json(msg="failed to lookup the deployment information for %s, error was: %s" % (name, str(e)))
+    return (changed, reserved_ip_address)
 
-def delete_service(module, azure):
+#    try:
+#        service = azure.get_hosted_service_properties(service_name=name)
+#        return (changed, service)
+#    except WindowsAzureError as e:
+#        module.fail_json(msg="failed to lookup the deployment information for %s, error was: %s" % (name, str(e)))
+
+def delete_ip_address(module, azure):
     """
-    Deletes a service
+    Deletes a reserved IP address
 
     module : AnsibleModule object
     azure: authenticated azure ServiceManagementService object
 
-    Not yet supported: handle deletion of attached data disks.
-
     Returns:
-        True if a service was deleted, false otherwise
+        True if a reserved IP address was deleted, false otherwise
     """
 
     name = module.params.get('name')
@@ -182,25 +188,25 @@ def delete_service(module, azure):
 
     changed = False
 
-    service = None
+    reserved_ip_address = None
     try:
-        service = service = azure.get_hosted_service_properties(service_name=name)
+        reserved_ip_address = azure.get_reserved_ip_address(name=name)
     except WindowsAzureMissingResourceError as e:
-        pass  # no such service
+        pass  # no such reserved ip address
     except WindowsAzureError as e:
-        module.fail_json(msg="failed to find the service, error was: %s" % str(e))
+        module.fail_json(msg="failed to find the reserved IP address, error was: %s" % str(e))
 
     # Delete service
-    if service:
+    if reserved_ip_address:
         changed = True
         try:
-            result = azure.delete_hosted_service(service_name=name)
+            result = azure.delete_reserved_ip_address(name=name)
             if (wait):
-                _wait_for_completion(azure, result, wait_timeout, "delete_hosted_service")
+                _wait_for_completion(azure, result, wait_timeout, "delete_reserved_ip_address")
         except WindowsAzureError as e:
-            module.fail_json(msg="failed to delete the service %s, error was: %s" % (name, str(e)))
+            module.fail_json(msg="failed to delete the reserved IP address %s, error was: %s" % (name, str(e)))
 
-    return changed, service
+    return changed, reserved_ip_address
 
 def get_azure_creds(module):
     # Check modul args for credentials, then check environment vars
@@ -222,12 +228,11 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             name=dict(),
-            hostname=dict(),
+            label=dict(),
             location=dict(choices=AZURE_LOCATIONS),
             subscription_id=dict(no_log=True),
             management_cert_path=dict(),
             state=dict(default='present', choices=['present', 'absent']),
-            affinity_group=dict(),
             wait=dict(type='bool', default=False),
             wait_timeout=dict(default=600),
             wait_timeout_redirects=dict(default=300)
@@ -244,17 +249,17 @@ def main():
         azure = ServiceManagementService(subscription_id, management_cert_path)
 
     if module.params.get('state') == 'absent':
-        (changed, service) = delete_service(module, azure)
+        (changed, reserved_ip_address) = delete_ip_address(module, azure)
 
     elif module.params.get('state') == 'present':
         # Changed is always set to true when provisioning new instances
         if not module.params.get('name'):
-            module.fail_json(msg='name parameter is required for new service')
-        if not module.params.get('location') and not module.params.get('affinity_group'):
-            module.fail_json(msg='location or affinity_group parameter is required for new service')
-        (changed, service) = create_service(module, azure)
+            module.fail_json(msg='name parameter is required for new reserved IP address')
+        if not module.params.get('location'):
+            module.fail_json(msg='location parameter is required for new reserved IP address')
+        (changed, reserved_ip_address) = create_ip_address(module, azure)
 
-    module.exit_json(changed=changed, service=json.loads(json.dumps(service, default=lambda o: o.__dict__)))
+    module.exit_json(changed=changed, reserved_ip_address=json.loads(json.dumps(reserved_ip_address, default=lambda o: o.__dict__)))
 
 
 class Wrapper(object):
